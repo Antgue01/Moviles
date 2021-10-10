0ffaf33fa2afa42f787ba6c9559803adc07a64ac
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2021-10-10 13:40:34 +0200

    UPDATED: Tablero y celdas
    Co-Authored-By: Antgue01 <48839258+Antgue01@users.noreply.github.com>
    Co-Authored-By: Will7461 <43720307+Will7461@users.noreply.github.com>

--- Practica1/PC/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/pc/Celda.java
+++ Practica1/PC/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/pc/Celda.java
@@ -1,5 +1,7 @@
 package es.fdi.ucm.gdv.vdism.maranwi.pc;
 
+import sun.util.resources.cldr.ext.CurrencyNames_to;
+
 //                  0     1      2
 enum TipoCelda { Azul, Rojo, Blanco}
 
@@ -9,18 +11,35 @@ public class Celda {
         _tipo = tipo;
     }
 
-    public void siguiente(){
-        if(!_esFicha)
-            _tipo = TipoCelda.values()[(_tipo.ordinal() + 1) % 3];
+    //Devuelve 0 si no hay cambios
+    //Devuelve 1 si se cambia una ficha blanca a otro color - se pierde una blanca en el tablero -
+    //Devuelve 2 si se cambia una ficha de otro color a blanca - se añade una blanca en el tablero -
+    public int cambiarFicha(boolean siguiente){
+        int returnCode = 0;
+        if(_esFicha){
+            if(_tipo == TipoCelda.Blanco)    returnCode = 1;
+
+            if(siguiente) _tipo = TipoCelda.values()[(_tipo.ordinal() + 1) % 3];
+            else _tipo = TipoCelda.values()[(_tipo.ordinal() + 2) % 3];
+
+            if(_tipo == TipoCelda.Blanco) returnCode = 2;
+            return returnCode;
+        }
+        return returnCode;
     }
 
-    public void anterior(){
-
-        //PONER BIEN LO DE LOS MODULOS -> ANTONIO xD
-        if(!_esFicha)
-            _tipo = TipoCelda.values()[(_tipo.ordinal() + 3) - 1];
+    public boolean getEsFicha(){
+        return _esFicha;
     }
 
-    boolean _esFicha;
-    TipoCelda _tipo;
+    public TipoCelda getTipoCelda(){
+        return _tipo;
+    }
+
+    public int getTipoCeldaAsInt(){
+        return _tipo.ordinal();
+    }
+
+    private boolean _esFicha;
+    private TipoCelda _tipo;
 }
--- Practica1/PC/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/pc/Tablero.java
+++ Practica1/PC/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/pc/Tablero.java
@@ -14,25 +14,34 @@ public class Tablero {
         _casillasAlto = height / filas;
         _casillasAncho = width / columnas;
 
-
-        _matriz = new int [_filas][_columnas];
+        _matrizSolucion = new int [_filas][_columnas];
         _matrizJuego = new Celda[_filas][_columnas];
+
+        _numFichasBlancas = 0;
     }
 
 
     /*
-        0 = Pared
-        1 = Numérico
-        2 = Azul
-        3 = Rojo
+        0 = Azul
+        1 = Rojo
      */
     public void rellenaMatrizResueltaRandom(){
-        for(int x = 0; x < _matriz[0].length; ++x){
-            for(int j = 0; j< _matriz[1].length; ++j){
-                _matriz[x][j] = (int) Math.random() * 2;
+        for(int x = 0; x < _matrizSolucion[0].length; ++x){
+            for(int j = 0; j< _matrizSolucion[1].length; ++j){
+                _matrizSolucion[x][j] = (int) Math.random() * 2;
+
 
                 boolean esFicha = ((int) Math.random() * 2 ) == 1;
-                Celda c = new Celda(esFicha, TipoCelda.values()[_matriz[x][j]]);
+                Celda c;
+
+                //Si es ficha se oculta su color, si no, se deja como está
+                if(esFicha){
+                    c = new Celda(esFicha, TipoCelda.Blanco);
+                    ++_numFichasBlancas;
+                }
+                else
+                    c = new Celda(esFicha, TipoCelda.values()[_matrizSolucion[x][j]]);
+
                 _matrizJuego[x][j] = c;
             }
         }
@@ -47,11 +56,39 @@ public class Tablero {
         return new Pair<Integer, Integer>(identificadorX, identificadorY);
     }
 
-    public void cambiarFicha(boolean siguiente, Pair<Integer, Integer> casilla){
+    //Auxiliares que hay que quitar, se ponen para completar onclick
+    public boolean leftClick(){   return true; }
 
+    //ESTAMOS SUPONIENDO QUE TODOS LOS CLICKS CAEN EN CASILLAS
+    public void onClick(float xPos, float yPos){
+        Pair<Integer, Integer> posicionFicha = identificaFicha(xPos, yPos);
+
+        //Identificar si es click izquierdo o click derecho (preguntar)
+        boolean siguiente = leftClick() ? true : false;
+
+        //0 = NO hay cambios. 1 = se quita blanca del tablero. 2 = se añade blanca al tablero
+        int result = _matrizJuego[posicionFicha.first][posicionFicha.second].cambiarFicha(siguiente);
+        if(result == 1) --_numFichasBlancas;
+        else if(result == 2 ) ++_numFichasBlancas;
+
+        if(_numFichasBlancas == 0 && compruebaSolucion()){
+            //SE HA GANADO LA PARTIDA -> HACER LO QUE SEA
+        }
     }
 
-    int _matriz[][];
+    private boolean compruebaSolucion(){
+        boolean esSolucion = true;
+        for(int x = 0; x < _matrizSolucion[0].length && esSolucion; ++x){
+            for(int j = 0; j < _matrizSolucion[1].length; ++j){
+                if(_matrizJuego[x][j].getEsFicha() && _matrizJuego[x][j].getTipoCeldaAsInt() != _matrizSolucion[x][j])
+                    esSolucion = false;
+            }
+        }
+        return esSolucion;
+    }
+
+    //La matriz solucíón solo guarda los colores de cada posición
+    int _matrizSolucion[][];
     Celda _matrizJuego[][];
     int _filas;
     int _columnas;
@@ -59,4 +96,6 @@ public class Tablero {
     float _casillasAlto;
     int _width;
     int _height;
+
+    int _numFichasBlancas;
 }
