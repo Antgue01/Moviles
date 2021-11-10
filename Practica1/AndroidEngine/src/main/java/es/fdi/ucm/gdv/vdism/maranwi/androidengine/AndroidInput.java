2071b092500aa8e4ce28d6c54603902a8441eeec
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2021-11-11 00:27:17 +0100

    Update AndroidInput.java

--- Practica1/AndroidEngine/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/androidengine/AndroidInput.java
+++ Practica1/AndroidEngine/src/main/java/es/fdi/ucm/gdv/vdism/maranwi/androidengine/AndroidInput.java
@@ -1,12 +1,57 @@
 package es.fdi.ucm.gdv.vdism.maranwi.androidengine;
 
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import es.fdi.ucm.gdv.vdism.maranwi.engine.Input;
 
-public class AndroidInput implements Input {
+public class AndroidInput implements Input, View.OnTouchListener {
+    public AndroidInput() {
+        _events = new ArrayList<TouchEvent>();
+    }
+
+
     @Override
     public List<TouchEvent> getTouchEvents() {
-        return null;
+        List<TouchEvent> auxEvents;
+        synchronized (this) {
+            auxEvents = new ArrayList<>(_events);
+            _events.clear();
+        }
+        return auxEvents;
     }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ArrayList<TouchEvent> events = new ArrayList<TouchEvent>();
+
+        TouchEvent.TouchType type = null;
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_UP:
+                type = TouchEvent.TouchType.liberacion;
+                break;
+            case MotionEvent.ACTION_DOWN:
+                type = TouchEvent.TouchType.pulsacion;
+                break;
+            case MotionEvent.ACTION_SCROLL:
+                type = TouchEvent.TouchType.desplazamiento;
+                break;
+        }
+        if (type != null) {
+            int id = event.getActionIndex();
+            MotionEvent.PointerCoords coords = new MotionEvent.PointerCoords();
+            event.getPointerCoords(id, coords);
+            TouchEvent e = new TouchEvent(type, (int)coords.x,(int) coords.y, id);
+            synchronized (this)
+            {
+                _events.add(e);
+            }
+            return true;
+        } else return false;
+    }
+    private List<TouchEvent> _events;
 }
