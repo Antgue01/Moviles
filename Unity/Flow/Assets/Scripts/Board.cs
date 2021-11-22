c46f300afb8f252fc4f87843b4d9e7e4bd3d2fe1
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2021-11-22 22:04:35 +0100

    Avances tablero

--- Unity/Flow/Assets/Scripts/Board.cs
+++ Unity/Flow/Assets/Scripts/Board.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+   
+
     [SerializeField]
     public GameObject gameBoxPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       // _current = BoxType.Empty;
     }
 
     // Update is called once per frame
@@ -23,10 +25,41 @@ public class Board : MonoBehaviour
     public void setMap(Map p)
     {
         _map = p;
+        _mapSize = _map.getRows() * _map.getCols();
+        _boxes = new GameObject[_mapSize];
+        configureBoard();
     }
 
-        
+    private void configureBoard()
+    {
+        for(int x = 0; x < _mapSize; x++)
+        {
+            GameObject go = Instantiate(gameBoxPrefab, new Vector3((float)0, 0, 0), Quaternion.identity) as GameObject;
+            go.transform.localScale = Vector3.one;
+            _boxes[x] = go;            
+        }
+    }
 
-    Map _map;
-    //Vector<GameBox> _box;
+    private void checkBridges()
+    {
+        for(int x = 0; x < _map.getBridges().Length; x++)
+        {
+
+        }
+    }
+
+    private void checkHollows()
+    {
+        for (int x = 0; x < _map.getBridges().Length; x++)
+        {
+
+        }
+    }
+
+    //private BoxType _current;
+    private int[] _board;
+    private Map _map;
+    private int _mapSize;
+    private GameObject[] _boxes;
+    private Image[] _sprites;
 }
--- Unity/Flow/Assets/Scripts/GameBox.cs
+++ Unity/Flow/Assets/Scripts/GameBox.cs
@@ -8,6 +8,18 @@ public class GameBox
     [SerializeField]
     public GameObject gameBox;
 
+    
+
+    public bool isBridge()
+    {
+        return _type == BoxType.Bridge;
+    }
+
+    public bool isHollow()
+    {
+        return _type == BoxType.Hollow;
+    }
+
     public void setImage(Sprite s)
     {
         _img = s;
@@ -25,8 +37,9 @@ public class GameBox
         gameBox.transform.position = _position;
     }
 
-
     private Color _color;
     private Sprite _img;
     private Vector2 _position;
+    private BoxType _type;
+    private enum BoxType { Bridge, Hollow, Flow, Empty }
 }
