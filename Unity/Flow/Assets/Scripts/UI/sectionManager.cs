f9ad122403b0ba783055a89a99b730d1ffa3b04f
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2021-11-23 23:36:28 +0100

    Habemus scroll

--- Unity/Flow/Assets/Scripts/UI/sectionManager.cs
+++ Unity/Flow/Assets/Scripts/UI/sectionManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class sectionManager : MonoBehaviour
 {
-    [SerializeField] RectTransform _scrollTransform;
+    [SerializeField] RectTransform _scrollContentTransform;
     [SerializeField] RectTransform _layoutZone;
     [SerializeField] GameObject _headerPrefab;
     [SerializeField] GameObject _levelLotPrefab;
@@ -18,7 +18,7 @@ public class sectionManager : MonoBehaviour
             //we instantiate the header of the section
             GameObject header = Instantiate<GameObject>(_headerPrefab, _layoutZone);
             HeaderManager headerManager = header.GetComponent<HeaderManager>();
-            totalHeight += header.GetComponent<RectTransform>().rect.yMax;
+            totalHeight += header.GetComponent<RectTransform>().rect.height;
             headerManager.setName(sections[i].SectionName);
             headerManager.setTheme(sections[i].themeColor);
             //we fill the progress data for each level lot and then we instantiate them
@@ -27,7 +27,7 @@ public class sectionManager : MonoBehaviour
             {
                 GameObject levelLotObject = Instantiate(_levelLotPrefab, _layoutZone);
                 LevelLotVisuals levelLotVisuals = levelLotObject.GetComponent<LevelLotVisuals>();
-                totalHeight += levelLotObject.GetComponent<RectTransform>().rect.yMax;
+                totalHeight += levelLotObject.GetComponent<RectTransform>().rect.height;
                 levelLotVisuals.setColor(sections[i].themeColor);
                 levelLotVisuals.setName(levellot.LevelLotName);
                 //todo CUANDO SEPAMOS GUARDAR EL PROGRESO QUE SE MIRE AQUI
@@ -35,7 +35,7 @@ public class sectionManager : MonoBehaviour
                 levelLotVisuals.setLevelsInfo(levellot.LevelLotFile.ToString().Split(separators,System.StringSplitOptions.RemoveEmptyEntries).Length, /*levelLot.playedLevels*/0);
             }
         }
-        //_scrollTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, totalHeight);
+        _scrollContentTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, totalHeight);
     }
 }
 
