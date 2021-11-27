ecab7306a593d833818c56b4ce9cc8a310277b45
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2021-11-27 11:43:11 +0100

    Avances Scroll (queda poder controlar la posición inicial en Y y que el scroll ocupe toda la pantalla

--- Unity/Flow/Assets/Scripts/UI/LevelLotDisplayer.cs
+++ Unity/Flow/Assets/Scripts/UI/LevelLotDisplayer.cs
@@ -22,24 +22,28 @@ public class LevelLotDisplayer : MonoBehaviour
         int numLevels = levels.Length;
         int numPages = numLevels / 30;
         float w = 0;
+        RectTransform levelTransform = null;
         for (int i = 0; i < numPages; i++)
         {
             Vector3 dir = new Vector3(1, 0, 0);
-            Vector3 pos = _gridParent.position + (dir * i * _canvasTransform.rect.width);
-            GameObject levelGridObject = Instantiate<GameObject>(_levelPagePrefab, pos, _gridParent.rotation, _gridParent);
-            LevelDisplayer lvlDisplayer=levelGridObject.GetComponent<LevelDisplayer>();
+            Vector3 pos = _scroll.position +
+                (Vector3.right * i * _canvasTransform.rect.width) + _startPoint.position.x * Vector3.right
+               /* + Vector3.up*_startPoint.position.y*/;
+            GameObject levelGridObject = Instantiate<GameObject>(_levelPagePrefab, pos, _scroll.rotation, _scroll);
+            LevelDisplayer lvlDisplayer = levelGridObject.GetComponent<LevelDisplayer>();
             lvlDisplayer.Display(lvlLot, i);
-            levelGridObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _scroll.rect.width);
-            levelGridObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _scroll.rect.height);
+            levelTransform = levelGridObject.GetComponent<RectTransform>();
+            levelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _scroll.rect.width);
+            levelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _scroll.rect.height);
             w += _canvasTransform.rect.width;
         }
-        w -= _canvasTransform.rect.width / 2;
+        w -= _canvasTransform.rect.width - levelTransform.rect.width;
         _scroll.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, w);
         _scroll.Translate(Vector3.right * w / 2);
     }
     [SerializeField] GameObject _levelPagePrefab;
-    [SerializeField] Transform _gridParent;
     [SerializeField] RectTransform _canvasTransform;
     [SerializeField] Text _sectionName;
     [SerializeField] RectTransform _scroll;
+    [SerializeField] RectTransform _startPoint;
 }
